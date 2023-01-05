c8d9a414e3bce26b2710b76d29ea2ea48c3b36e7
shiba-msmr <shiba307@icloud.com>
2023-01-05 23:57:27 +0900

    alembicでmigrateできるように

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 
 ### back
 - FastAPI
+- SQLAlchemy
+- Alembic
--- back/api/db/database.py
+++ back/api/db/database.py
@@ -2,7 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://user:pass@db:3306/kanri_db"
+import os
+
+DB_USER = os.environ.get("MYSQL_USER")
+DB_PASS = os.environ.get("MYSQL_PASSWORD")
+DB_HOST = os.environ.get("MYSQL_HOST")
+DB_PORT = os.environ.get("MYSQL_PORT")
+DB_NAME = os.environ.get("MYSQL_DATABASE")
+
+SQLALCHEMY_DATABASE_URL = f"mysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
